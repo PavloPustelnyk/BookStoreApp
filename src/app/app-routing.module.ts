72900db033fb2cff5bb92d5b96131e6f62faeead
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-28 12:06:18 +0200

    Add client routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { BookInfoComponent } from './components/book-info/book-info.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
+import { AuthorInfoComponent } from './components/author-info/author-info.component';
+import { AuthorsListComponent } from './components/authors-list/authors-list.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { BookCategoriesComponent } from './components/book-categories/book-categories.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'categories', component: BookCategoriesComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'user/info', component: UserInfoComponent},
+  {path: 'books/:id', component: BookInfoComponent},
+  {path: 'books/page/:pageNo', component: BooksListComponent},
+  {path: 'books/page/:pageNo/:categoryId', component: BooksListComponent},
+  {path: 'authors/:id', component: AuthorInfoComponent},
+  {path: 'authors/page/:pageNo', component: AuthorsListComponent},
+  {path: 'books', redirectTo: 'books/page/1', pathMatch: 'full'},
+  {path: 'authors', redirectTo: 'authors/page/1', pathMatch: 'full'},
+  {path: '404', component: NotFoundComponent},
+  {path: '**', redirectTo: '404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
