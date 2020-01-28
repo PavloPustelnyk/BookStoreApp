3d351dbe2922927fbddca524d335d77209a7d9f8
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-28 22:04:18 +0200

    Change api port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44397/api'
+  apiUrl: 'https://localhost:5001/api'
 };
 
 /*
@@ -14,4 +14,5 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';
+// Included with Angular CLI.
