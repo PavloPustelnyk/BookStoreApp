e42742879c3fa4b54329c57efe6cbf53204848ad
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-28 12:12:43 +0200

    Add book service

---
+++ src/app/_services/book/book.service.ts
@@ -0,0 +1,69 @@
+import { environment } from './../../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { BookDetailed } from '../../_models/_detailed/book-detailed';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  public pagesCount: number;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPagesCount() {
+    return this.httpClient.get<any>(`${environment.apiUrl}/books/pages`)
+      .pipe(map((pages: number) => {
+        this.pagesCount = pages;
+        return pages;
+      },
+      error => {
+        console.log(error);
+      }));
+  }
+
+  getCategoryPagesCount(categoryId: number) {
+    return this.httpClient.get<any>(`${environment.apiUrl}/categories/${categoryId}/books/pages`)
+      .pipe(map((pages: number) => {
+        this.pagesCount = pages;
+        return pages;
+      },
+      error => {
+        console.log(error);
+      }));
+  }
+
+  getBooksPage(page: number) {
+    return this.httpClient.get<BookDetailed[]>(`${environment.apiUrl}/books/page/${page}`)
+      .pipe(map((books: BookDetailed[]) => {
+        return books;
+      },
+      error => {
+        console.log(error);
+      }));
+  }
+
+  getBook(id: number) {
+    return this.httpClient.get<BookDetailed>(`${environment.apiUrl}/books/${id}`)
+      .pipe(map((book: BookDetailed) => {
+        return book;
+      },
+      error => {
+        console.log(error);
+      }));
+  }
+
+  getBooksPageOfCategory(page: number, categoryId: number) {
+    return this.httpClient.get<BookDetailed[]>(`${environment.apiUrl}/categories/${categoryId}/books/page/${page}`)
+      .pipe(map((books: BookDetailed[]) => {
+        return books;
+      },
+      error => {
+        console.log(error);
+      }));
+  }
+
+  
+}
