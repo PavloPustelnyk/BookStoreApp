adc24990459d60a35b00d639239452f825f65870
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-28 12:07:48 +0200

    Add logout method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserDetailed } from './_models/_detailed/user-detailed';
+import { AuthenticationService } from './_services/authentication/authentication.service';
+import { LoggedSubject } from './_models/_detailed/logged-subject';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'bookstore-client';
+  styleUrls: ['./app.component.css']})
+export class AppComponent implements OnInit {
+
+    currentUser: LoggedSubject;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    }
+
+    ngOnInit() {}
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
 }
