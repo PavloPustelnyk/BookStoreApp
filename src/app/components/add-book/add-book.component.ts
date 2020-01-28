c5dba5a39d9659c66482e79220065754dcf47a2b
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-28 12:13:36 +0200

    Add add-book-component

---
+++ src/app/components/add-book/add-book.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-book',
+  templateUrl: './add-book.component.html',
+  styleUrls: ['./add-book.component.css']
+})
+export class AddBookComponent implements OnInit {
+
+  postForm: FormGroup;
+  submitted = false;
+  loading = false;
+  error = '';
+  postError = false;
+
+  constructor(private bookService: ,
+              private formBuilder: FormBuilder,
+              private route: ActivatedRoute,
+              private router: Router) {
+    if (this.authService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z\d$@$!%*?&].{8,}')]],
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      birthDate: ['', [Validators.required]]
+    });
+
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    const user = new UserRegister();
+
+    user.email = this.registerForm.controls.email.value;
+    user.password = this.registerForm.controls.password.value;
+    user.firstName = this.registerForm.controls.firstName.value;
+    user.lastName = this.registerForm.controls.lastName.value;
+    user.birthDate = this.registerForm.controls.birthDate.value;
+
+    this.loading = true;
+
+    this.authService.register(user).pipe(first()).subscribe(data => {
+      this.loading = false;
+      this.router.navigate(['/login']);
+    },
+    error => {
+      this.loading = false;
+      console.log(error);
+      this.registerError = true;
+    });
+  }
+
+  get f() { return this.registerForm.controls; }
+
+}
