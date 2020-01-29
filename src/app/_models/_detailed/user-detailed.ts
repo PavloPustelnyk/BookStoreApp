99581ef3a64f70db9095ab338cd17c95fca11300
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-30 01:04:28 +0200

    Add user`s liked books to UserDetailed model

--- src/app/_models/_detailed/user-detailed.ts
+++ src/app/_models/_detailed/user-detailed.ts
@@ -1,6 +1,9 @@
+import { Book } from '../_simplified/book';
+
 export class UserDetailed {
   public email: string;
   public firstName: string;
   public lastName: string;
   public birthDate: Date;
+  public likedBooks: Book[];
 }
