6f6a3e24e716fd8e796b75bcbc8bb44f7bc8b73a
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-30 01:11:08 +0200

    Realize author-list component

--- src/app/components/authors-list/authors-list.component.ts
+++ src/app/components/authors-list/authors-list.component.ts
@@ -13,21 +13,45 @@ export class AuthorsListComponent implements OnInit {
 
   public page: number;
 
+  public pagesCount: number;
+
+  public authors: AuthorDetailed[];
+
   public loading = true;
 
   constructor(public authorService: AuthorService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.loadPagesCount();
     this.loadAuthorPage();
   }
 
   private loadAuthorPage() {
     this.page = Number(this.route.snapshot.paramMap.get('pageNo'));
-    this.authorService.getBooksPage(this.page).pipe(first()).subscribe((data: AuthorDetailed[]) => {
-      this.loading = false;
-    }, error => {
-      console.log(error);
-    });
+
+    this.authorService
+      .getBooksPage(this.page)
+      .subscribe((data: AuthorDetailed[]) => {
+        this.loading = false;
+        this.authors = data;
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  private loadPagesCount() {
+    this.authorService
+      .getPagesCount()
+      .subscribe((data: number) => {
+        this.pagesCount = data;
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
+  counter(i: number) {
+    return new Array(i);
   }
 }
