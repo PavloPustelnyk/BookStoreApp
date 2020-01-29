27455e333aa46e16865d79e8c09012e47a1dc1ec
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-30 01:13:53 +0200

    Add 'Admin' button on navigation bar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../_services/authentication/authentication.service';
 import { Router } from '@angular/router';
+import { Roles } from '../../constants/Roles';
+import { LoggedSubject } from '../../_models/_detailed/logged-subject';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,15 +11,32 @@ import { Router } from '@angular/router';
 })
 export class NavBarComponent implements OnInit {
 
+  public userRole: string;
+
+  public adminRole: string;
+
+  public currUser: LoggedSubject;
+
   constructor(public authService: AuthenticationService,
-              private router: Router) { }
+              private router: Router) {
+    this.userRole = Roles.UserRole;
+    this.adminRole = Roles.AdminRole;
+  }
 
   ngOnInit() {
+    this.authService.currentUser.subscribe((data: LoggedSubject) => this.currUser = data);
   }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  isAuthenticated(): boolean {
+    if (this.authService.currentUserValue) {
+      return true;
+    }
+    return false;
   }
 
 }
