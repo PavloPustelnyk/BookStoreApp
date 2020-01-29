eec34e6a72d40d8eaa23db0a4723e0329684809d
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-30 01:03:01 +0200

    Add check for status 400

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -14,14 +14,18 @@ export class ErrorInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
             if (err.status === 401) {
-                // auto logout if 401 response returned from api
-                this.authenticationService.logout();
-                this.router.navigate(['/login']);
+              // auto logout if 401 response returned from api
+              this.authenticationService.logout();
+              this.router.navigate(['/login']);
             }
 
             if (err.status === 404) {
               this.router.navigate(['/404']);
             }
+
+            if (err.status === 400) {
+            }
+            console.log(request, err, err.error);
 
             const error = err.error.message || err.statusText;
             return throwError(error);
