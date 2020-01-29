8592a825a790f358b8e64507227b99efee876558
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-30 01:12:05 +0200

    Add styles

--- src/app/components/book-list-item/book-list-item.component.ts
+++ src/app/components/book-list-item/book-list-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BookDetailed } from '../../_models/_detailed/book-detailed';
+import { stringify } from 'querystring';
+import { concat } from 'rxjs';
 
 @Component({
   selector: 'app-book-list-item',
@@ -13,6 +15,21 @@ export class BookListItemComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getBookImage() {
+    if (!this.book.bookImage) {
+      return 'assets/images/default_book.png';
+    }
+    return this.book.bookImage;
+  }
+
+  getBookAuthor() {
+    return this.book.author.firstName + ' ' + this.book.author.lastName;
+  }
+
+  getBookRating() {
+    return (this.book.summaryRating / this.book.reviewCount).toFixed(1);
   }
 
 }
