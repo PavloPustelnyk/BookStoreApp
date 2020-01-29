b1b9c7cd02807e0cd8146f029b48c44044610426
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-01-30 01:11:45 +0200

    Add styles, likeBook method

--- src/app/components/book-info/book-info.component.ts
+++ src/app/components/book-info/book-info.component.ts
@@ -3,6 +3,10 @@ import { BookService } from '../../_services/book/book.service';
 import { ActivatedRoute } from '@angular/router';
 import { BookDetailed } from '../../_models/_detailed/book-detailed';
 import { first } from 'rxjs/operators';
+import { AuthenticationService } from '../../_services/authentication/authentication.service';
+import { Book } from 'src/app/_models/_simplified/book';
+import { UserService } from '../../_services/user/user.service';
+import { UserDetailed } from '../../_models/_detailed/user-detailed';
 
 @Component({
   selector: 'app-book-info',
@@ -11,17 +15,38 @@ import { first } from 'rxjs/operators';
 })
 export class BookInfoComponent implements OnInit {
 
+  private user: UserDetailed;
+
   public bookId: number;
 
   public book: BookDetailed;
 
   public loading = true;
 
+  public likeLoading = false;
+
   constructor(public bookService: BookService,
+              public authService: AuthenticationService,
+              public userService: UserService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.loadBook();
+
+    if (this.authService.currentUserValue) {
+      this.loadUser();
+    }
+  }
+
+  private loadUser() {
+    this.userService
+      .getUserInfo()
+      .subscribe((data: UserDetailed) => {
+        this.user = data;
+      },
+      error => {
+        console.log(error);
+      });
   }
 
   private loadBook() {
@@ -32,5 +57,46 @@ export class BookInfoComponent implements OnInit {
     }, error => {
       console.log(error);
     });
+  }
+
+  getBookImage() {
+    if (!this.book.bookImage) {
+      return 'assets/images/default_book.png';
+    }
+    return this.book.bookImage;
+  }
+
+  getBookAuthor() {
+    return this.book.author.firstName + ' ' + this.book.author.lastName;
+  }
+
+  getBookRating() {
+    return (this.book.summaryRating / this.book.reviewCount).toFixed(1);
+  }
+
+  likedBook(): boolean {
+
+    console.log(this.bookId);
+
+    if (this.user && this.user.likedBooks) {
+      return this.user.likedBooks.find(b => b.id === this.bookId) != null;
+    }
+
+    return false;
+  }
+
+  addToFavorites() {
+    this.likeLoading = true;
+
+    this.userService
+      .likeBook(this.bookId)
+      .subscribe((data: UserDetailed) => {
+        this.likeLoading = false;
+        this.user = data;
+      },
+      error => {
+        this.likeLoading = false;
+        console.log(error);
+      });
   }
 }
