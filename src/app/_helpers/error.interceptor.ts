7bd66c10ea7174e5de932a0a40cfe8eebd77651f
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-02-03 12:38:22 +0200

    Refactore helpers

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -4,15 +4,21 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthenticationService } from '../_services/authentication/authentication.service';
 import { Router } from '@angular/router';
+import { NotifierService } from 'angular-notifier';
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
     constructor(private authenticationService: AuthenticationService,
-                private router: Router) { }
+                private router: Router,
+                private notifier: NotifierService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
+            if (err.status === 0) {
+              alert('Server is not responding');
+            }
+
             if (err.status === 401) {
               // auto logout if 401 response returned from api
               this.authenticationService.logout();
@@ -27,8 +33,7 @@ export class ErrorInterceptor implements HttpInterceptor {
             }
             console.log(request, err, err.error);
 
-            const error = err.error.message || err.statusText;
-            return throwError(error);
+            return throwError(err);
         }));
     }
 }
