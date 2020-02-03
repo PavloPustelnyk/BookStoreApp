cb39f09214ec7a1607db11d2cf4c2bb5e60ec8e0
pavlo.pustelnyk.pz.2017@lpnu.ua <pavlo.pustelnyk.pz.2017@lpnu.ua>
2020-02-03 12:49:02 +0200

    Add admin form group

--- src/app/components/admin-panel/admin-panel.component.ts
+++ src/app/components/admin-panel/admin-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-panel',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPanelComponent implements OnInit {
 
-  constructor() { }
+  adminForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              ) { }
 
   ngOnInit() {
+    this.adminForm = this.formBuilder.group({
+      action: ['category']
+    });
   }
+
+  get f() { return this.adminForm.controls; }
 
 }
